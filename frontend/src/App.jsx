db5054f4a512312f2914d8fdbbe85f482210ff08
gargominy <gargominy.portfolio@gmail.com>
2026-07-29 10:21:10 +0000

    Wed, Jul 29, 2026, 10:21 AM +00:00

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
-import Message from './'
+import Message from './Message/Message'
 import './App.css'
 
 function App() {
 
   return (
     <>
+      <Message url="http://localhost:5000/api/hello" />
     </>
   )
 }
