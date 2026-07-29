cdd5431b447fb85f3d10d722ba92bb56ee699a5f
gargominy <gargominy.portfolio@gmail.com>
2026-07-29 14:42:57 +0000

    Wed, Jul 29, 2026, 2:42 PM +00:00

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,10 @@
 import React, { useState, useEffect } from 'react'
-import Message from './Message/Message'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1>Hello, React!</h1>
-      <Message url="http://localhost:5000/api/hello" />
     </>
   )
 }
