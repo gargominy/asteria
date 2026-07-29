0485fc83471c7bb78ee1e67506eee571bf524307
gargominy <gargominy.portfolio@gmail.com>
2026-07-29 10:04:44 +0000

    Wed, Jul 29, 2026, 10:04 AM +00:00

--- backend/app.py
+++ backend/app.py
@@ -7,4 +7,4 @@ def hello():
     return jsonify({"message": "Hello, from Flask!"})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5010)
+    app.run(debug=True, host='0.0.0.0', port=5000)
