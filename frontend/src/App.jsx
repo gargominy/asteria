30450cc7bc795dc703199fcd010d5c0ee0d4531c
gargominy <gargominy.portfolio@gmail.com>
2026-07-29 10:20:33 +0000

    Wed, Jul 29, 2026, 10:20 AM +00:00

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Message from './'
 import './App.css'
 
 function App() {
--- frontend/src/Message/Message.jsx
+++ frontend/src/Message/Message.jsx
@@ -5,9 +5,15 @@ import './App.css'
 function Message(props) {
   const [message, setMessage] = useState('')
 
+  useEffect(() => {
+    fetch(props.url)
+      .then(response => response.json())
+      .then(data => setMessage(data.message))
+  }, [props.url])
+
   return (
     <>
-      <p>{props.text}</p>
+      <p>{message}</p>
     </>
   )
 }
